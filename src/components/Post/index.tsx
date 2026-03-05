f03303fd07038811a52761730ecd669807e41212
Felipe Elias <felipedaniel.me@gmail.com>
2026-03-05 13:32:47 -0300

    fix: fix bug that makes the view more comments button flick when clicking to open the comments section

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -105,21 +105,19 @@ export function Post({ id, author, content, likesAmount, commentsAmount, publish
   async function handleCreateComment() {
     setPostCommentsAmount(prev => prev + 1)
     setPage(1)
-    setHasMore(true)
-    await fetchComments(1)
+    await fetchComments(page)
   }
 
   async function handleDeleteComment() {
     setPostCommentsAmount(prev => prev - 1)
     setPage(1)
-    await fetchComments(1)
+    await fetchComments(page)
   }
 
   async function handleOpenComments() {
     if (comments.length === 0) {
       setPage(1)
-      setHasMore(true)
-      await fetchComments(1)
+      await fetchComments(page)
     }
   }
 
